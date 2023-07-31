7857749823a54e893bc46941037af14f579b3405
Hatake <ratishatirishvili55@gmail.com>
2023-07-31 20:22:36 +0400

    Case3

--- src/test/java/ValidatingFirstBookTest.java
+++ src/test/java/ValidatingFirstBookTest.java
@@ -1,34 +1,24 @@
 import Steps.BooksSteps;
 import Steps.ValidatingBookDataSteps;
-import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
-import io.qameta.allure.Story;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.ConfigForTests;
 
 import static com.codeborne.selenide.Selenide.open;
 
 @Epic("Select first book")
 @Feature("Validate parameters")
-public class ValidatingFirstBookTest {
+public class ValidatingFirstBookTest extends ConfigForTests {
 
     BooksSteps booksSteps = new BooksSteps();
     ValidatingBookDataSteps validatingBookDataSteps = new ValidatingBookDataSteps();
 
-    @BeforeMethod(alwaysRun = true)
-    @Story("Opening url for all tests")
-    @Description("Maximize browsers windows")
-    public void setUp() {
-        open("https://demoqa.com/books");
-        WebDriverRunner.getWebDriver().manage().window().maximize();
-
-    }
-
     @Test()
     @Description("Selecting first book and checking UI and data parameters are equal")
     public void FirstBookTest() {
+        open("/books");
         booksSteps.clickFirstBook()
                 .firstBookIsbn();
 
